8b1e90ccb3e702ef7c961cc2c733bc9a26dc392a
Pedro Del Gallego <pedro.delgallego@gmail.com>
2011-10-07 16:15:44 +0200

    [orm jquery] wrap the code using the module pattern

--- katas/orm-jquery/orm-persistence.js
+++ katas/orm-jquery/orm-persistence.js
@@ -1,61 +1,63 @@
-// Empty collection of instances
-Model.extend({
-  created: function(){
-    this.records = {};
-  }
-});
-
-Model.records = {}
-
-Model.include({
-  newRecord: true,
-
-  create: function(){
-    if ( !this.id ) this.id = Math.guid();
-    this.newRecord = false;
-    this.parent.records[this.id] = this.dup();
-  },
-
-  update: function(){
-    this.newRecord = false;
-    this.parent.records[this.id] = this.dup();
-  },
-
-  save: function(){
-    this.newRecord ? this.create() : this.update();
-  },
-
-  dup: function(){
-    return jQuery.extend(true, {}, this);
-  }
-})
-
-// Finders
-Model.extend({
-  find: function(id){
-    var record = this.records[id]
-    return record.dup();
-  }
-})
-
-Model.extend({
-  populate: function(values){
-    // Reset model & records
-    this.records = {};
-
-    for (var i=0, length = values.length; i < length; i++) {
-      var record = this.init(values[i]);
-      record.newRecord = false;
-      this.records[record.id] = record;
+(function($, exports){
+  // Empty collection of instances
+  Model.extend({
+    created: function(){
+      this.records = {};
     }
-  }
-});
+  });
 
-// Id generator
-// see http://www.broofa.com/2008/09/javascript-uuid-function/
-Math.guid = function(){
-  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-    var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
-    return v.toString(16);
-  }).toUpperCase();
-};
+  Model.records = {}
+
+  Model.include({
+    newRecord: true,
+
+    create: function(){
+      if ( !this.id ) this.id = Math.guid();
+      this.newRecord = false;
+      this.parent.records[this.id] = this.dup();
+    },
+
+    update: function(){
+      this.newRecord = false;
+      this.parent.records[this.id] = this.dup();
+    },
+
+    save: function(){
+      this.newRecord ? this.create() : this.update();
+    },
+
+    dup: function(){
+      return jQuery.extend(true, {}, this);
+    }
+  })
+
+  // Finders
+  Model.extend({
+    find: function(id){
+      var record = this.records[id]
+      return record.dup();
+    }
+  })
+
+  Model.extend({
+    populate: function(values){
+      // Reset model & records
+      this.records = {};
+
+      for (var i=0, length = values.length; i < length; i++) {
+        var record = this.init(values[i]);
+        record.newRecord = false;
+        this.records[record.id] = record;
+      }
+    }
+  });
+
+  // Id generator
+  // see http://www.broofa.com/2008/09/javascript-uuid-function/
+  Math.guid = function(){
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+      var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
+      return v.toString(16);
+    }).toUpperCase();
+  };
+})(jQuery, window)
--- katas/orm-jquery/orm.js
+++ katas/orm-jquery/orm.js
@@ -1,67 +1,69 @@
 /*
 
 */
+(function($, exports){
+  // Object.create() takes one argument, a prototype object, and returns
+  // a new object with the specified prototype object. In other words,
+  // you give it an object, and it returns a new one, inheriting from
+  // the one you specified.
+  // see http://javascript.crockford.com/prototypal.html
+  if (typeof Object.create !== "function"){
+    Object.create = function(o) {
+      function F() {}
+      F.prototype = o;
+      return new F();
+    };
+  }
 
-// Object.create() takes one argument, a prototype object, and returns
-// a new object with the specified prototype object. In other words,
-// you give it an object, and it returns a new one, inheriting from
-// the one you specified.
+  var Model = {
+    inherited: function(){},
+    created: function(){},
+    prototype: { init: function(){} },
 
-// see http://javascript.crockford.com/prototypal.html
-if (typeof Object.create !== "function"){
-  Object.create = function(o) {
-    function F() {}
-    F.prototype = o;
-    return new F();
+    // The create() function returns a new object, inheriting from the
+    // Model object; we’ll use this for creating new models. The init()
+    // function returns a new object, inheriting from Model.prototype
+    create: function(){
+      var object = Object.create(this);
+      object.parent = this;
+      object.prototype = object.fn = Object.create(this.prototype);
+      object.created();
+      this.inherited(object);
+      return object;
+    },
+
+    init: function(){
+      var instance = Object.create(this.prototype);
+      instance.parent = this;
+      instance.init.apply(instance, arguments);
+      return instance;
+    },
+
+    extend: function(o){
+      var extend = Object.extend || $.extend;
+      var extended = o.extended;
+      extend(this, o);
+      if (extended) extended(this);
+    },
+
+    include: function(o){
+      var extend = Object.extend || $.extend;
+      var included = o.included;
+      extend(this.prototype, o);
+      if (included) included(this);
+    }
   };
-}
 
-var Model = {
-  inherited: function(){},
-  created: function(){},
-  prototype: { init: function(){} },
+  Model.include({
+    init: function(atts) {
+      if (atts) this.load(atts);
+    },
 
-  // The create() function returns a new object, inheriting from the
-  // Model object; we’ll use this for creating new models. The init()
-  // function returns a new object, inheriting from Model.prototype
-  create: function(){
-    var object = Object.create(this);
-    object.parent = this;
-    object.prototype = object.fn = Object.create(this.prototype);
-    object.created();
-    this.inherited(object);
-    return object;
-  },
-
-  init: function(){
-    var instance = Object.create(this.prototype);
-    instance.parent = this;
-    instance.init.apply(instance, arguments);
-    return instance;
-  },
-
-  extend: function(o){
-    var extend = Object.extend || $.extend;
-    var extended = o.extended;
-    extend(this, o);
-    if (extended) extended(this);
-  },
-
-  include: function(o){
-    var extend = Object.extend || $.extend;
-    var included = o.included;
-    extend(this.prototype, o);
-    if (included) included(this);
-  }
-};
-
-Model.include({
-  init: function(atts) {
-    if (atts) this.load(atts);
-  },
-
-  load: function(attributes){
-    for(var name in attributes)
-      this[name] = attributes[name];
-  }
-})
+    load: function(attributes){
+      for(var name in attributes)
+        this[name] = attributes[name];
+    }
+  })
+  
+  exports.Model = Model
+})(jQuery, window)
